f1a90bea7c581959324228205bbe218062a65229
lfabre <fabre.leo@gmail.com>
2026-06-12 01:16:29 +0200

    branding: vendor Dubplex

--- plugin/DistrhoPluginInfo.h
+++ plugin/DistrhoPluginInfo.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DISTRHO_PLUGIN_BRAND       "Nexus"
+#define DISTRHO_PLUGIN_BRAND       "Dubplex"
 #define DISTRHO_PLUGIN_NAME        "EQ13"
 #define DISTRHO_PLUGIN_URI         "https://github.com/lfabre/eq13-dpf"
 #define DISTRHO_PLUGIN_CLAP_ID     "fr.nexus.eq13"
